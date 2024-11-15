35d0b715019c58bec3cc9294e9b3f57dc0c0e69a
TrixieM10 <trixiesantos884@gmail.com>
2024-11-16 01:31:27 +0800

    Add files via upload

---
+++ storage/framework/views/056909ecd606510994aaf4fd48ac5be1.php
@@ -0,0 +1,17 @@
+
+
+
+
+<?php $__env->startSection('content'); ?>
+    <h1><?php echo e($product->name); ?></h1>
+    <img src="<?php echo e(asset('storage/' . $product->image)); ?>" alt="Product Image" class="img-fluid">
+    <p><?php echo e($product->description); ?></p>
+    <a href="<?php echo e(route('products.edit', $product)); ?>" class="btn btn-primary">Edit</a>
+    <form action="<?php echo e(route('products.destroy', $product)); ?>" method="POST" style="display:inline;">
+        <?php echo csrf_field(); ?>
+        <?php echo method_field('DELETE'); ?>
+        <button type="submit" class="btn btn-danger">Delete</button>
+    </form>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\lunas_mediran\resources\views/products/show.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/7dee65dcc4e09321bac1e16019957ec2.php
@@ -0,0 +1,24 @@
+
+
+
+
+<?php $__env->startSection('content'); ?>
+    <h1>Products</h1>
+    <a href="<?php echo e(route('products.create')); ?>" class="btn btn-primary">Add Product</a>
+    <div class="row mt-3">
+        <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="<?php echo e(asset('storage/' . $product->image)); ?>" class="card-img-top" alt="Product Image">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo e($product->name); ?></h5>
+                        <p class="card-text"><?php echo e($product->description); ?></p>
+                        <a href="<?php echo e(route('products.show', $product)); ?>" class="btn btn-secondary">View</a>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\lunas_mediran\resources\views/products/index.blade.php ENDPATH**/ ?>
